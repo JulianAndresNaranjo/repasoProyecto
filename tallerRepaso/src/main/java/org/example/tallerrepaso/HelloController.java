e8fc9c9b866ef513f0224fcc9371b6eb58807c30
JulianAndresNaranjo <142056387+JulianAndresNaranjo@users.noreply.github.com>
2024-02-18 17:41:29 -0500

    Update HelloController.java

--- tallerRepaso/src/main/java/org/example/tallerrepaso/HelloController.java
+++ tallerRepaso/src/main/java/org/example/tallerrepaso/HelloController.java
@@ -1,14 +1,36 @@
 package org.example.tallerrepaso;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button btnEnterAdmin;
+
+    @FXML
+    public void EnterAdmin(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("enterAdmin-view.fxml"));
+            Parent root = loader.load();
+            EnterAdminController controlador = loader.getController();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(scene);
+            stage.showAndWait();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
